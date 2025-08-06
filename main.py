e243a775de403bc759e1aaa04e77d5c1a523a52e
cooperifyyou <super8cooper.game@gmail.com>
2025-08-05 21:01:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from discord.ext import commands, tasks
 import requests
 import os
 from datetime import datetime, timezone, timedelta
+from tasks import loop
 
 LOG_CHANNEL_ID = os.environ.get('LOGCHANNELID')
 TRELLO_KEY = os.environ.get('APIKEY')
